fc392a1ac3aafcc6c990dfeeb79280e80d8bdc7d
Dennis247 <denghazi619@gmail.com>
2020-05-13 04:01:59 +0100

    first commit

---
+++ Blog/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blog.Data;
+using Blog.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+        public HomeController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Post()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit()
+        {
+            return View(new Post());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Post post)
+        {
+            _appDbContext.Posts.Add(post);
+           await  _appDbContext.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
